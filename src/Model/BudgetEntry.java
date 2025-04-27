92edf58b54e3ab876c2c7b4e1bf26b049809e655
Benjamin Lyon <missle59@gmail.com>
2025-04-27 19:47:49 -0400

    v0.1.0 (“add model classes”)

---
+++ src/Model/BudgetEntry.java
@@ -0,0 +1,29 @@
+package Model;
+
+import java.math.BigDecimal;
+
+public class BudgetEntry {
+    private String category;
+    private BigDecimal amount;
+
+    public BudgetEntry(String category, BigDecimal amount) {
+        this.category = category;
+        this.amount = amount;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+}
---
+++ src/Model/SpendingEntry.java
@@ -0,0 +1,40 @@
+package Model;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class SpendingEntry {
+    private LocalDate date;
+    private String category;
+    private BigDecimal amount;
+
+    public SpendingEntry(LocalDate date, String category, BigDecimal amount) {
+        this.date = date;
+        this.category = category;
+        this.amount = amount;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+}
